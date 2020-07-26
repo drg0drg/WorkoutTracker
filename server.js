eccf9aedd4b15427d84d659a8729f7dcfcb9aab5
drg0drg <60710786+drg0drg@users.noreply.github.com>
2020-07-26 21:41:50 +0100

    api-routes, html-routes, heroku db added

--- server.js
+++ server.js
@@ -6,15 +6,24 @@ const logger = require("morgan");
 
 
 // Defining the port
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 // Tell the app to use express and use public folder for front end
+const app = express();
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://userDrg:userDrg12345@ds153096.mlab.com:53096/heroku_18ktcb46", { useNewUrlParser: true });
+
+
+app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/html-routes.js"));
+
 
 
 // Listening to port defined above
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
 });
+
